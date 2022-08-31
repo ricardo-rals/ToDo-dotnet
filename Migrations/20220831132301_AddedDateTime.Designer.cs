4b5fac998a12417d2f24362b5a2abce3059d80d8
ricardo.ralds <ricardo.rals.dev@gmail.com>
2022-08-31 10:23:51 -0300

    Adicionando migration de datetime

--- Migrations/20220831132301_AddedDateTime.Designer.cs
+++ Migrations/20220831132301_AddedDateTime.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MeuToDo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MeuToDo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220831132301_AddedDateTime")]
+    partial class AddedDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
--- Models/Todo.cs
+++ Models/Todo.cs
@@ -5,5 +5,6 @@ public class Todo
 	public int Id { get; set; }
 	public string Title { get; set; }
 	public bool Done { get; set; }
+	public DateTime Date { get; set; } = DateTime.Now;
 	
 }
