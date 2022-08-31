e0877c6b16cb47f524f8fcadefedb7c6f6eb7c99
ricardo.ralds <ricardo.rals.dev@gmail.com>
2022-08-31 11:32:49 -0300

    Pegando o item pelo ID

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -17,10 +17,25 @@ namespace MeuToDo.Controllers
             [FromServices]AppDbContext context
         )
         {
-            var todos = await context.Todos.AsNoTracking().ToListAsync();
+            var todos = await context
+                .Todos
+                .AsNoTracking()
+                .ToListAsync();
             return Ok(todos);
         }
 
-        
+        [HttpGet]
+        [Route(template:"todos/{id}")]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromServices] AppDbContext context,
+            [FromRoute] int id
+        )
+        {
+            var todo = await context
+                .Todos
+                .AsNoTracking()
+                .FirstOrDefaultAsync( x => x.Id == id);
+            return todo == null ? NotFound() : Ok(todo);
+        }
     }
 }
